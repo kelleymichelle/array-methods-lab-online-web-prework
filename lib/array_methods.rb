9eb606ff855ba159111e049384216b7911895c8d
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-12 02:25:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
-  array = ["array", "element"]
-    array.include? ("element")
+  array = [array, element]
+    array.include? (element)
 end
 
 def using_sort(array)
