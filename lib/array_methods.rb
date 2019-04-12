7a064b93eb92e37607948b85eb57dd49471d3d70
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-12 02:03:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,11 +6,12 @@ end
 
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.sort
+    array.sort
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.reverse
 end
 
 def using_first(array)
